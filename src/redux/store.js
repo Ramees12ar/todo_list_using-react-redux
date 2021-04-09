21605980b2d2155e0fac82ad0c7f86329b078c13
Ramees12ar <ramees.ar04@gmail.com>
2021-04-09 17:34:30 +0530

    localstorage added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,32 @@
 import { createStore} from 'redux';
 import {reducer} from './reducer';
 
-export let store = createStore(reducer);
+//export let store = createStore(reducer);
+
+function saveToLocalStorage(state) {
+    try {
+      const serialisedState = JSON.stringify(state);
+      localStorage.setItem("localData", serialisedState);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  function loadFromLocalStorage() {
+    try {
+      const serialisedState = localStorage.getItem("localData");
+      if (serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    } catch (e) {
+      console.warn(e);
+      return undefined;
+    }
+  }
+
+  const store = createStore(reducer, loadFromLocalStorage());
+
+// listen for store changes and use saveToLocalStorage to
+// save them to localStorage
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
